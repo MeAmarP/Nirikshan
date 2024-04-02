5a893016d7284a5af2d628b9c83e6e9c2c039e1c
MeAmarP <appotdar05@gmail.com>
2024-04-03 00:17:27 +0530

    init commit for count analytics

---
+++ src/analytics/count.py
@@ -0,0 +1,24 @@
+from configs import AppConfig
+
+
+class CountAnalytics:
+    def __init__(self):
+        self.target_class_labels = AppConfig.detector_class_labels
+        self.classwise_count = {}  # stores objects to be tracked with their counts
+        self.init_counts()  # initialize counts for each class label
+
+    def init_counts(self):        
+        for label in self.target_class_labels:
+            if label not in self.classwise_count:
+                self.classwise_count[label] = 0
+
+    def update(self, detection_output):
+        # Iterate over all detected objects
+        for obj in detection_output:
+            # Get the object's class label
+            label = obj['label']
+            
+            # If the object is a target to be tracked...
+            if label in self.target_class_labels:
+                # Increment its count
+                self.classwise_count[label] += 1
