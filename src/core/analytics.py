be4c0514ba6fe41e2e36f926d6ab3100c4f21f4c
MeAmarP <appotdar05@gmail.com>
2024-04-04 00:56:24 +0530

    minor modifications

--- src/core/analytics.py
+++ src/core/analytics.py
@@ -4,24 +4,19 @@ from configs import AppConfig
 class CountAnalytics:
     def __init__(self):
         self.target_class_labels = AppConfig.detector_class_labels
-        self.classwise_count = {}  # stores objects to be tracked with their counts
+        self.classwise_count_curr = {}  # stores objects to be tracked with their counts
         self.init_counts()  # initialize counts for each class label
 
     def __str__(self) -> str:
-        return " ".join(f"{k} {v}" for k,v in self.classwise_count.items())
+        return " ".join(f"{k} {v}" for k,v in self.classwise_count_curr.items())
 
     def init_counts(self):        
         for label in self.target_class_labels:
-            if label not in self.classwise_count:
-                self.classwise_count[label] = 0
+            if label not in self.classwise_count_curr:
+                self.classwise_count_curr[label] = 0
 
-    def update(self, detection_output):
-        # Iterate over all detected objects
-        for obj in detection_output:
-            # Get the object's class label
-            label = obj['label']
-            
-            # If the object is a target to be tracked...
-            if label in self.target_class_labels:
-                # Increment its count
-                self.classwise_count[label] += 1
+    def update(self, tracked_objects, label):            
+        # If the object is a target to be tracked...
+        if label in self.target_class_labels:
+            # Increment its count
+            self.classwise_count_curr[label] = len(tracked_objects)
