d204b3fd9c05f9f55c96630cd266ab2d9f29683d
meamarp <appotdar05@gmail.com>
2024-10-24 23:12:50 +0530

    udpated test as per the module changes

--- tests/test_detector.py
+++ tests/test_detector.py
@@ -4,19 +4,19 @@ from detector import ObjectDetector
 import os
 from pathlib import Path
 
-# ! TODO --> FIX THIS to run smoothly
 
 def test_load_model():
     # Test if it correctly loads the model from .cfg and .weights files
-    model_cfg = str(Path(os.getcwd()) / '..' / 'models' / 'yolov3.cfg')
-    model_weights = str(Path(os.getcwd()) / '..' / 'models' / 'yolov3.weights')
-    class_file = str(Path(os.getcwd())/ '..' / 'assets' / 'coco.names')
-    obj = ObjectDetector(model_cfg, model_weights, class_file)
+
+    obj = ObjectDetector('yolov3')
     assert isinstance(obj.net, cv2.dnn_Net)  # Assuming that net attribute is of type dnn_Net after loading the model
 
 def test_detect():
     # Test if it correctly detects objects in an image
     obj = ObjectDetector()
-    img = cv2.imread('/data/1.jpg')
+    # * NOTE Make sure path below is absolute path to the image file 
+    # ! TODO Write a code to get absolute path to input image file
+    path_to_img_file ='data/1.jpg' 
+    img = cv2.imread(path_to_img_file)
     detections = obj.detect(img)
     assert len(detections) >= 0  # It could be any positive integer, depending on the objects detected in the image
