fe3b88e8e5a523b4a83b70bf2657dad5e975ecac
meamarp <appotdar05@gmail.com>
2024-10-24 23:11:50 +0530

    improvements for video file path and other things

--- src/main.py
+++ src/main.py
@@ -13,16 +13,14 @@ from utils import (display_analytics, display_detections, display_faces,
 
 
 def main(path_to_vid_file: str):
-    # ! TODO Add code to get video file path from cli
-    # ! Check if input file exist
     filepath = Path(path_to_vid_file)
     if not filepath.exists():
         print("Invalid File path, Check if file exists at location???")
         # ! EXIT FROM APPLICATION DUE TO INVALID FILE PATH
-        exit()
+        exit(1)
         
 
-    with cv2.VideoCapture("/data/office.mp4") as cap:
+    with cv2.VideoCapture(str(filepath)) as cap:
         if not cap.isOpened():
             raise IOError("Could not open video file.")
         # If successful, continue with processing the video frames
@@ -42,52 +40,52 @@ def main(path_to_vid_file: str):
         # -------------------------------------------------------
 
         while True:
-                try:
-                    # Read a frame from the video
-                    ret, frame = cap.read()
+            try:
+                # Read a frame from the video
+                ret, frame = cap.read()
+                
+                # If the 'ret' value is False (i.e., there are no more frames to read)
+                if not ret:
+                    print("No frames to read.")
+                    break
+
+                # Detect objects
+                detections = yolov3_detector.detect(frame)
+
+                # Detect faces
+                face_detections = face_detector.detect(frame)
+
+                if len(detections) > 0:
+
+                    # byte-tracker expects detection format as numpy array of shape (N, 5), 
+                    # where N is the number of detections and each detection has format [x1, y1, x2, y2, confidence]  
+                    np_detections = np.array([np.concatenate((np.array(det['box']).astype(np.float16), np.array([det['confidence']]).astype(np.float16))) for det in detections])
                     
-                    # If the 'ret' value is False (i.e., there are no more frames to read)
-                    if not ret:
-                        print("No frames to read.")
-                        break
-
-                    # Detect objects
-                    detections = yolov3_detector.detect(frame)
-
-                    # Detect faces
-                    face_detections = face_detector.detect(frame)
-
-                    if len(detections) > 0:
-
-                        # byte-tracker expects detection format as numpy array of shape (N, 5), 
-                        # where N is the number of detections and each detection has format [x1, y1, x2, y2, confidence]  
-                        np_detections = np.array([np.concatenate((np.array(det['box']).astype(np.float16), np.array([det['confidence']]).astype(np.float16))) for det in detections])
-                        
-                        tracked_objects = tracker.update(np_detections)
-                        
-                        # ** Analytics **
-                        count_analytics.update(tracked_objects, 'person')
-                        display_analytics(frame, count_analytics)
-
-                        # draw detections
-                        # display_detections(frame=frame, detections=detections)
-
-                        # draw tracked objects
-                        display_tracked_ids(frame=frame, tracked_objects=tracked_objects)
-
-                    if len(face_detections) > 0:
-                        display_faces(frame, face_detections)
-
-                    # Display the current frame
-                    cv2.imshow('Video', frame)
+                    tracked_objects = tracker.update(np_detections)
                     
-                    # Break the loop on 'q' key press
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
-                except Exception as e:
-                    print(f"An error occurred while trying to process the video frames: {e}")
-                    traceback.print_exc()
-                    continue
+                    # ** Analytics **
+                    count_analytics.update(tracked_objects, 'person')
+                    display_analytics(frame, count_analytics)
+
+                    # draw detections
+                    display_detections(frame=frame, detections=detections)
+
+                    # draw tracked objects
+                    display_tracked_ids(frame=frame, tracked_objects=tracked_objects)
+
+                if len(face_detections) > 0:
+                    display_faces(frame, face_detections)
+
+                # Display the current frame
+                cv2.imshow('Video', frame)
+                
+                # Break the loop on 'q' key press
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            except Exception as e:
+                print(f"An error occurred while trying to process the video frames: {e}")
+                traceback.print_exc()
+                continue
     
     cv2.destroyAllWindows()
 
@@ -97,5 +95,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get Path to valid Video file..")
     parser.add_argument('fpath',type=str, help="Provide path to Video file" )
     args = parser.parse_args()
-    filepath = args.path
-    main(path_to_vid_file=filepath)
+    fpath = args.path
+    main(path_to_vid_file=fpath)
